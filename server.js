e63095887d61b9fa33240748295323d9d2e351a4
Your Name <you@example.com>
2026-04-01 23:37:31 +0700

    adding gitignore

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
